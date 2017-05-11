1a660657dee523a0a48cb0f3b44d0f10a7cc9c70
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-05-11 14:17:58 -0300

    script commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 0.0.0.0:$PORT app:hello
+web: gunicorn -w 4 app:app
--- publish.sh
+++ publish.sh
@@ -1,3 +1,7 @@
+# deploy to github
 git add --all
 git commit -m "script commit"
 git push -u origin master
+
+# deploy to heroku
+git push heroku master
